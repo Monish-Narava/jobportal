35251e354ce7b35d5528a199f58197fc6deff030
Monish-Narava <nmonishmonish@gmail.com>
2026-01-08 10:20:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def get_connection():
         database=os.environ.get("MYSQLDATABASE"),
         port=int(os.environ.get("MYSQLPORT", 3306)),
         cursorclass=pymysql.cursors.DictCursor,
-        autocommit=True
+        
     )
 
 # ---------------- HOME ----------------
@@ -198,5 +198,6 @@ def logout():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
